bcc20b5048499830adf27ee2428a2e332c827d88
M-Umar4002 <134300686+M-Umar4002@users.noreply.github.com>
2024-02-01 22:16:23 +0500

    Update q04.cpp

--- Labs/01/q04.cpp
+++ Labs/01/q04.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int maxximumArea(int arr[], int size) {
+int maximumArea(int arr[], int size) {
     
-    int maxArea = -1;
+    int maxArea = 0;
     
-    for(int i = 0; i < size; i++) {
-        for(int j = i + 1; j < size; j++) {
-            if ((j-i) * min(arr[i], arr[j]) > maxArea) maxArea = (j-i) * min(arr[i], arr[j]);
-        }
-    }
+    for(int i = 0; i < size; i++) for(int j = i + 1; j < size; j++) maxArea = max(maxArea, (j-i) * min(arr[i], arr[j]));
     
     return maxArea;
 }
 
-int main(){
+int main() {
+    
     int size = 9, height[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
     
-    cout << maxximumArea(height, size);
+    cout << maximumArea(height, size);
 
     return 0;
 }

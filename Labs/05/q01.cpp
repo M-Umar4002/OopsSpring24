4f5648641d53d53af3aa1615eeb6ce5802085ea1
M-Umar4002 <134300686+M-Umar4002@users.noreply.github.com>
2024-03-04 03:07:09 +0500

    Create q01.cpp

---
+++ Labs/05/q01.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+class Square {
+    float sideLength, area;
+    static float allAreas;
+    
+    public : 
+        Square() {}
+        
+        Square(float sideLength) {
+            this->sideLength = sideLength;
+        }
+        
+        void setSideLength(float length) {
+            sideLength = length;
+        }
+        
+        void calculateArea() {
+            area = sideLength*sideLength;
+            allAreas += area;
+        }
+        
+        float getSideLength() {
+            return sideLength;
+        }
+        
+        float getArea() {
+            return area;
+        }
+        
+        float getAllAreas() {
+            return allAreas;
+        }
+};
+
+float Square :: allAreas = 0.0;
+
+int main() {
+    
+    Square s[3];
+    s[0] = Square(3), s[1] = Square(4), s[2] = Square(5);
+    
+    for(int i = 0; i < 3; i++) {
+        s[i].calculateArea();
+        cout << "After calling calculateArea function for square " << i+1 << endl << "Area : " << s[i].getArea() << endl << "All areas : " << s[i].getAllAreas() << endl << endl;
+    }
+
+    return 0;
+}

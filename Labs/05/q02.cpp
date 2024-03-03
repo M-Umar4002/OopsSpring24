7a25e231036504e35e3d66c0beca1c7bc534485f
M-Umar4002 <134300686+M-Umar4002@users.noreply.github.com>
2024-03-04 04:13:39 +0500

    Create q02.cpp

---
+++ Labs/05/q02.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class LoanHelper {
+    const float interestRate;
+    float amount;
+    int amountOfMonths;
+    
+    public : 
+        LoanHelper(float interest, float am, int months) : interestRate(interest), amount(am), amountOfMonths(months) {}
+        
+        void calculateLoan() {
+            float repay = (amount/amountOfMonths)+((amount/amountOfMonths)*interestRate);
+            cout << "You have to pay " << repay << " for " << amountOfMonths << " months to repay your loan." << endl;
+        }
+};
+
+int main() {
+    
+    LoanHelper l(0.25/100, 50000, 10);
+    
+    l.calculateLoan();
+
+    return 0;
+}

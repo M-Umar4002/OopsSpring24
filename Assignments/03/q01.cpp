819d0787ffef575995026f9668a7487a0cb63f22
M-Umar4002 <134300686+M-Umar4002@users.noreply.github.com>
2024-05-05 19:49:39 +0500

    Update q01.cpp

--- Assignments/03/q01.cpp
+++ Assignments/03/q01.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
@@ -7,109 +6,77 @@ class Medicine {
     string name, formula, manufactureDate, expirationDate;
     int retailPrice;
     
-public : 
-    Medicine(string name, string formula, string manufactureDate, string expirationDate, int retailPrice) : name(name), formula(formula), manufactureDate(manufactureDate), expirationDate(expirationDate), retailPrice(retailPrice) {}
-    
-    void setName(string name) {
-        this->name = name;
-    }
-    
-    void setFormula(string formula) {
-        this->formula = formula;
-    }
-    
-    void setManufactureDate(string manufactureDate) {
-        this->manufactureDate = manufactureDate;
-    }
-    
-    void setExpirationDate(string expirationDate) {
-        this->expirationDate = expirationDate;
-    }
-    
-    void setRetailPrice(int retailPrice) {
-        this->retailPrice = retailPrice;
-    }
-    
-    string getName() const {
-        return name;
-    }
-    
-    string getFormula() const {
-        return formula;
-    }
-    
-    string getManufactureDate() const {
-        return manufactureDate;
-    }
-    
-    string getExpirationDate() const {
-        return expirationDate;
-    }
-    
-    int getRetailPrice() const {
-        return retailPrice;
-    }
-    
-    virtual void printDetails() {
-        cout << "Name : " << name << endl << "Formula : " << formula << endl << "Manufacture Date : " << manufactureDate << endl << "Expiration Date : " << expirationDate << endl << "Retail price : " << retailPrice << endl;
-    }
+    public : 
+        Medicine(string name, string formula, string manufactureDate, string expirationDate, int retailPrice) : name(name), formula(formula), manufactureDate(manufactureDate), expirationDate(expirationDate), retailPrice(retailPrice) {}
+        
+        void setName(string name) const;
+        void setFormula(string formula) const;
+        void setManufactureDate(string manufactureDate) const;
+        void setExpirationDate(string expirationDate) const;
+        void setRetailPrice(int retailPrice) const;
+        
+        string getName() const;
+        string getFormula() const;
+        string getManufactureDate() const;
+        string getExpirationDate() const;
+        int getRetailPrice() const;
+        
+        virtual void printDetails() {}
+        
+        bool operator==(const Medicine &med2) {
+            return this->expirationDate.substr(6, 4) == med2.expirationDate.substr(6, 4);
+        }
 };
 
 class Tablet : public Medicine {
-    int sucroseLevel;
+    float sucroseLevel;
     
-public :
-    Tablet(string name, string formula, string manufactureDate, string expirationDate, int retailPrice, int sucroseLevel) : Medicine(name, formula, manufactureDate, expirationDate, retailPrice), sucroseLevel(sucroseLevel) {}
-    
-    void printDetails() {
-        Medicine::printDetails();
-        cout << "Sucrose Level : " << sucroseLevel << endl;
-    }
+    public :
+        Tablet(string name, string formula, string manufactureDate, string expirationDate, int retailPrice, float sucroseLevel) : Medicine(name, formula, manufactureDate, expirationDate, retailPrice), sucroseLevel(sucroseLevel) {}
+        
+        void printDetails() {}
 };
 
 class Capsule : public Medicine {
     float absorptionPercentage;
     
-public :
-    Capsule(string name, string formula, string manufactureDate, string expirationDate, int retailPrice, float absorptionPercentage) : Medicine(name, formula, manufactureDate, expirationDate, retailPrice), absorptionPercentage(absorptionPercentage) {}
-    
-    void printDetails() {
-        Medicine::printDetails();
-        cout << "Absorption Percentage : " << absorptionPercentage << endl;
-    }
+    public :
+        Capsule(string name, string formula, string manufactureDate, string expirationDate, int retailPrice, float absorptionPercentage) : Medicine(name, formula, manufactureDate, expirationDate, retailPrice), absorptionPercentage(absorptionPercentage) {}
+        
+        void printDetails() {}
 };
 
 class Syrup : public Medicine {
-    Syrup(string name, string formula, string manufactureDate, string expirationDate, int retailPrice) : Medicine(name, formula, manufactureDate, expirationDate, retailPrice) {}
-    
-    void printDetails() {
-        Medicine::printDetails();
-    }
+    public :
+        Syrup(string name, string formula, string manufactureDate, string expirationDate, int retailPrice) : Medicine(name, formula, manufactureDate, expirationDate, retailPrice) {}
+        
+        void printDetails() {}
 };
 
 class Pharmacist {
-public :
-    void searchMedicine(Medicine medicines[], int totalMedicines, Medicine medicine) {
-        for(int i = 0; i < totalMedicines; i++) {
-            if(medicines[i].getFormula() == medicine.getFormula()) {
-                medicines[i].printDetails();
-            }
-        }
-    }
+    public :
+        void searchMedicine(Medicine medicines[], int totalMedicines, Medicine medicine);
 };
 
 class Counter {
-public :
-    void searchMedicine(Medicine medicines[], int totalMedicines, Medicine medicine) {
-        for(int i = 0; i < totalMedicines; i++) {
-            if(medicines[i].getName() == medicine.getName()) {
-                medicines[i].printDetails();
-            }
-        }
-    }
+    static float revenue;
+    
+    public :
+        void searchMedicine(Medicine medicines[], int totalMedicines, Medicine medicine);
+        
+        void updateRevenue(float amount);
 };
 
 int main() {
+    Tablet tablet("Panadol", "HGFT3", "10/10/2023", "09/09/2024", 10, 0.3);
+    
+    Capsule capsule("Omeprazole", "JUFG53", "08/08/2022", "09/09/2024", 10, 60.87);
+    
+    Syrup syrup("Omeprazole", "JUFG53", "04/04/2024", "02/02/2025", 10);
+    
+    tablet == capsule ? cout << "Both tablet and cabsule will expire in same year\n" : cout << "Both tablet and cabsule will not expire in same years\n";
+    
+    tablet == syrup ? cout << "Both tablet and syrup will expire in same year\n" : cout << "Both tablet and syrup will not expire in same years\n";
 
     return 0;
 }
